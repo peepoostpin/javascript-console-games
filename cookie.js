36a6dcc19e985371a1b27f108feed7e47a18c34a
berrybonk <91695764+peepoostpin@users.noreply.github.com>
2022-12-28 11:57:22 +0000

    Create cookie.js

---
+++ cookie.js
@@ -0,0 +1,96 @@
+let cookies = 0;
+let clickMultiplier = 1;
+let ovenMultiplier = 1;
+let factoryMultiplier = 1;
+let ovenInterval;
+let factoryInterval;
+
+const clickCookie = () => {
+  cookies += 1 * clickMultiplier;
+  console.log(`You have ${cookies} cookie(s).`);
+};
+
+const bakeCookies = () => {
+  cookies += 1 * ovenMultiplier;
+  console.log(`Your Cookie Oven is baking cookies. You now have ${cookies} cookie(s).`);
+};
+
+const produceCookies = () => {
+  cookies += 1 * factoryMultiplier;
+  console.log(`Your Cookie Factory is producing cookies. You now have ${cookies} cookie(s).`);
+};
+
+console.log("Welcome to Cookie Clicker! To click the cookie, type 'c' and press Enter. To open the shop, type 's' and press Enter. To quit the game, type 'e' and press Enter.");
+
+let gameRunning = true;
+
+while (gameRunning) {
+  const input = prompt("What would you like to do?");
+  switch (input) {
+    case "c":
+      clickCookie();
+      break;
+    case "s":
+      console.log("Welcome to the shop! Here are the available items:");
+      console.log("1. Automatic Cookie Clicker (100 cookies)");
+      console.log("2. Cookie Oven (250 cookies)");
+      console.log("3. Cookie Factory (500 cookies)");
+      console.log("Type the number of the item you want to buy and press Enter.");
+      console.log("Type 'exit' to exit the shop.");
+      let shopRunning = true;
+      while (shopRunning) {
+        const shopInput = prompt("What would you like to do?");
+        switch (shopInput) {
+          case "1":
+            if (cookies >= 100) {
+                            console.log("You have purchased the Automatic Cookie Clicker. Cookies will now be automatically collected for you.");
+              cookies -= 100;
+              clickMultiplier = 2;
+              shopRunning = false;
+            } else {
+              console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
+            }
+            break;
+          case "2":
+            if (cookies >= 250) {
+              console.log("You have purchased the Cookie Oven. Your cookie production has increased.");
+              cookies -= 250;
+              ovenMultiplier = 2;
+              ovenInterval = setInterval(bakeCookies, 1000);
+              shopRunning = false;
+            } else {
+              console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
+            }
+            break;
+          case "3":
+            if (cookies >= 500) {
+              console.log("You have purchased the Cookie Factory. Your cookie production has greatly increased.");
+              cookies -= 500;
+              factoryMultiplier = 5;
+              factoryInterval = setInterval(produceCookies, 1000);
+              shopRunning = false;
+            } else {
+              console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
+            }
+            break;
+          case "exit":
+            console.log("Exiting the shop...");
+            shopRunning = false;
+            break;
+          default:
+            console.log("Invalid input. Please try again.");
+            break;
+        }
+      }
+      break;
+    case "e":
+      console.log("Thanks for playing Cookie Clicker! Goodbye.");
+      clearInterval(ovenInterval);
+      clearInterval(factoryInterval);
+      gameRunning = false;
+      break;
+    default:
+      console.log("Invalid input. Please try again.");
+      break;
+  }
+}
