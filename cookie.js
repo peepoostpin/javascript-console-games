964ef674d12bb1158a98dbe995a3ff9dce9293b7
berrybonk <91695764+peepoostpin@users.noreply.github.com>
2022-12-28 14:49:39 +0000

    beautifie

--- cookie.js
+++ cookie.js
@@ -1,5 +1,4 @@
 // BTW SO LIKE THERE AR A FEW BUGS SO LIKE YE LIEK OVENS AND THAT DONT WORK YET BUT I WILL FIX IT BECAUSE IM COOL
-
 let cookies = 10000;
 let clickMultiplier = 1;
 let ovenMultiplier = 1;
@@ -8,27 +7,27 @@ let ovenInterval;
 let factoryInterval;
 
 const clickCookie = () => {
-  if (!powerOff) {
-    cookies += 1 * clickMultiplier;
-    console.log(`You have ${cookies} cookie(s).`);
-  } else {
-    console.log("Power is currently OFF. You cannot click cookies.");
-  }
+    if (!powerOff) {
+        cookies += 1 * clickMultiplier;
+        console.log(`You have ${cookies} cookie(s).`);
+    } else {
+        console.log("Power is currently OFF. You cannot click cookies.");
+    }
 };
 
 const bakeCookies = () => {
-  console.log("Bake Cookies function called");
-  if (!powerOff && gameRunning) {
-    cookies += 1 * ovenMultiplier;
-    console.log(`Your Cookie Oven is baking cookies. You now have ${cookies} cookie(s).`);
-  }
+    console.log("Bake Cookies function called");
+    if (!powerOff && gameRunning) {
+        cookies += 1 * ovenMultiplier;
+        console.log(`Your Cookie Oven is baking cookies. You now have ${cookies} cookie(s).`);
+    }
 };
 
 const produceCookies = () => {
-  if (!powerOff && gameRunning) {
-    cookies += 1 * factoryMultiplier;
-    console.log(`Your Cookie Factory is producing cookies. You now have ${cookies} cookie(s).`);
-  }
+    if (!powerOff && gameRunning) {
+        cookies += 1 * factoryMultiplier;
+        console.log(`Your Cookie Factory is producing cookies. You now have ${cookies} cookie(s).`);
+    }
 };
 
 
@@ -38,82 +37,82 @@ let gameRunning = true;
 let powerOff = false;
 
 while (gameRunning) {
-  const input = prompt("What would you like to do?");
-  switch (input) {
-    case "c":
-      clickCookie();
-      break;
-    case "p":
-      if (powerOff) {
-         powerOff = false;
-         console.log("Power is now ON. You can click cookies again.");
-      } else {
-        powerOff = true;
-        console.log("Power is now OFF. You can only use ovens and factories to produce cookies.");
-      }
-      break;
-    case "s":
-      console.log("Welcome to the shop! Here are the available items:");
-      console.log("1. Automatic Cookie Clicker (100 cookies)");
-      console.log("2. Cookie Oven (250 cookies)");
-      console.log("3. Cookie Factory (500 cookies)");
-      console.log("Type the number of the item you want to buy and press Enter.");
-      console.log("Type 'exit' to exit the shop.");
-      let shopRunning = true;
-      while (shopRunning) {
-        const shopInput = prompt("What would you like to do?");
-        switch (shopInput) {
-          case "1":
-            if (cookies >= 100) {
-              console.log("You have purchased the Automatic Cookie Clicker. Cookies will now be automatically collected for you.");
-              cookies -= 100;
-              clickMultiplier += 2;
-              shopRunning = false;
+    const input = prompt("What would you like to do?");
+    switch (input) {
+        case "c":
+            clickCookie();
+            break;
+        case "p":
+            if (powerOff) {
+                powerOff = false;
+                console.log("Power is now ON. You can click cookies again.");
             } else {
-              console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
+                powerOff = true;
+                console.log("Power is now OFF. You can only use ovens and factories to produce cookies.");
             }
             break;
-          case "2":
-            if (cookies >= 250) {
-              console.log("You have purchased the Cookie Oven. Your cookie production has increased.");
-              cookies -= 250;
-              ovenMultiplier += 2;
-              ovenInterval = setInterval(bakeCookies, 1000);
-              shopRunning = false;
-            } else {
-              console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
+        case "s":
+            console.log("Welcome to the shop! Here are the available items:");
+            console.log("1. Automatic Cookie Clicker (100 cookies)");
+            console.log("2. Cookie Oven (250 cookies)");
+            console.log("3. Cookie Factory (500 cookies)");
+            console.log("Type the number of the item you want to buy and press Enter.");
+            console.log("Type 'exit' to exit the shop.");
+            let shopRunning = true;
+            while (shopRunning) {
+                const shopInput = prompt("What would you like to do?");
+                switch (shopInput) {
+                    case "1":
+                        if (cookies >= 100) {
+                            console.log("You have purchased the Automatic Cookie Clicker. Cookies will now be automatically collected for you.");
+                            cookies -= 100;
+                            clickMultiplier += 2;
+                            shopRunning = false;
+                        } else {
+                            console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
+                        }
+                        break;
+                    case "2":
+                        if (cookies >= 250) {
+                            console.log("You have purchased the Cookie Oven. Your cookie production has increased.");
+                            cookies -= 250;
+                            ovenMultiplier += 2;
+                            ovenInterval = setInterval(bakeCookies, 1000);
+                            shopRunning = false;
+                        } else {
+                            console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
+                        }
+                        break;
+                    case "3":
+                        if (cookies >= 500) {
+                            console.log("You have purchased the Cookie Factory. Your cookie production has greatly increased.");
+                            cookies -= 500;
+                            factoryMultiplier += 5;
+                            factoryInterval = setInterval(produceCookies, 1000);
+                            shopRunning = false;
+                        } else {
+                            console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
+                        }
+                        break;
+                    case "exit":
+                        console.log("Exiting the shop...");
+                        shopRunning = false;
+                        break;
+                    default:
+                        console.log("Invalid input. Please try again.");
+                        break;
+                }
             }
             break;
-          case "3":
-            if (cookies >= 500) {
-              console.log("You have purchased the Cookie Factory. Your cookie production has greatly increased.");
-              cookies -= 500;
-              factoryMultiplier += 5;
-              factoryInterval = setInterval(produceCookies, 1000);
-              shopRunning = false;
-            } else {
-              console.log("You don't have enough cookies to buy this item. Please choose another item or type 'exit' to exit the shop.");
-            }
+        case "e":
+            console.log("Thanks for playing Cookie Clicker! Goodbye.");
+            clearInterval(ovenInterval);
+            clearInterval(factoryInterval);
+            powerOff = false;
+            gameRunning = false;
             break;
-          case "exit":
-            console.log("Exiting the shop...");
-            shopRunning = false;
-            break;
-          default:
+        default:
             console.log("Invalid input. Please try again.");
             break;
-        }
-      }
-      break;
-    case "e":
-      console.log("Thanks for playing Cookie Clicker! Goodbye.");
-      clearInterval(ovenInterval);
-      clearInterval(factoryInterval);
-      powerOff = false;
-      gameRunning = false;
-      break;
-    default:
-      console.log("Invalid input. Please try again.");
-      break;
-  }
+    }
 }
